5b46e5413fdae56b459c01b33ef4ab837f3f18f6
colin <cpade@gofundme.com>
2018-10-19 15:45:30 -0700

    remove blocking result() call from future

--- request_futures.py
+++ request_futures.py
@@ -1,15 +1,19 @@
 from requests_futures.sessions import FuturesSession
 from concurrent.futures import ThreadPoolExecutor
+from time import sleep
 
 urls = ['http://speedtest.ftp.otenet.gr/files/test1Mb.db'] * 50
 
-session = FuturesSession(executor=ThreadPoolExecutor(max_workers=100))
+session = FuturesSession(executor=ThreadPoolExecutor(max_workers=500))
 
 
 def find_multi_reviews(urls):
-    resp = [session.get(url).result().status_code for url in urls]
-    print(resp)
+    resp = [session.get(url) for url in urls]
     return resp
 
 
-find_multi_reviews(urls)
+results = find_multi_reviews(urls)
+for i, future in enumerate(results):
+	while future.running():
+		sleep(.1)
+	print(f'future {i} {(future.result().status_code)}')
